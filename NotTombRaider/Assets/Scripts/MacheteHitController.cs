7b959fbd63fae448c90dbda72528a8772ee0fef9
Tristan Bowler <tristan.bowler@gmail.com>
2020-07-29 17:24:47 -0600

    can kill enemies with a machete

---
+++ NotTombRaider/Assets/Scripts/MacheteHitController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MacheteHitController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public MacheteController macheteController;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (macheteController.isSwinging)
+        {
+
+
+            if (other.gameObject.tag == "Enemy")
+            {
+                other.gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("Hit " + other.gameObject.name);
+            }
+        }
+    }
+
+}
