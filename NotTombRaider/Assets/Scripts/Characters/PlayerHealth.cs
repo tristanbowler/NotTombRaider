1b0b8d9d6d26b2cb6787280b835e9501133c2a62
Tristan Bowler <tristan.bowler@gmail.com>
2020-10-12 23:41:39 -0600

    start on health

---
+++ NotTombRaider/Assets/Scripts/Characters/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int health;
+
+    public void DealDammage(int dammage)
+    {
+        health -= dammage;
+    }
+
+    public void Heal(int hp)
+    {
+        health += hp;
+    }
+
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
