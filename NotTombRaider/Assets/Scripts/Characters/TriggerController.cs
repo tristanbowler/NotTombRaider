a36103be4a0c03ccc294869629a410dea464e880
Tristan Bowler <tristan.bowler@gmail.com>
2020-11-19 14:07:18 -0700

    Skeles that do stuff in a new dungeon and some randomization

---
+++ NotTombRaider/Assets/Scripts/Characters/TriggerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            other.GetComponent<SkeletonController>().enabled = true;
+            other.GetComponent<HideAndShow>().Show();
+        }
+    }
+}
---
+++ NotTombRaider/Assets/Scripts/Enemies/HideAndShow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAndShow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hide();
+    }
+
+    public void Hide()
+    {
+        Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rends)
+        {
+            r.enabled = false;
+        }
+    }
+    public void Show()
+    {
+        Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rends)
+        {
+            r.enabled = true;
+        }
+    }
+}
