bc0edb732580bcc1e8beaea790a56ac897c406e1
Tristan Bowler <tristan.bowler@gmail.com>
2020-12-14 23:02:23 -0700

    some animation fuckery

---
+++ NotTombRaider/Assets/Prefabs/Enemies/SwitchBool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBool : MonoBehaviour
+{
+    public Animator animator;
+    public string boolName;
+    public bool setTo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+        if(animator.GetBool(boolName) == !setTo)
+        {
+            animator.SetBool(boolName, setTo);
+        }
+    }
+}
