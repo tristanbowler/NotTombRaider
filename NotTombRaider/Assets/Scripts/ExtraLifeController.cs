f6b6ff87582cd3bf49430eee11a02cf87ea3d517
Tristan Bowler <tristan.bowler@gmail.com>
2020-12-19 01:40:34 -0700

    Working on extras

---
+++ NotTombRaider/Assets/Scripts/ExtraLifeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLifeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2"))
+        {
+
+        }
+    }
+}
