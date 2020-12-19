961c56a05acad68c7e78800feac3fec6a09985b3
Tristan Bowler <tristan.bowler@gmail.com>
2020-12-19 02:15:57 -0700

    progress on lives

--- NotTombRaider/Assets/Scripts/Characters/LivesController.cs
+++ NotTombRaider/Assets/Scripts/Characters/LivesController.cs
@@ -6,10 +6,41 @@ public class LivesController : MonoBehaviour
 {
     public GameObject Thief;
     public GameObject Fighter;
+    public GameObject livesPanel;
+    public GameObject livesPrefab;
+    public int currentLives;
+    public int startLives;
+    public int maxLives;
+
 
     private void Start()
     {
         Thief = GameObject.FindGameObjectWithTag("Player1");
         Fighter = GameObject.FindGameObjectWithTag("Player2");
+        for(int i=0; i<startLives; i++)
+        {
+            AddLife();
+        }
     }
+
+    public void AddLife()
+    {
+        if(currentLives < maxLives)
+        {
+            currentLives++;
+            Instantiate(livesPrefab, livesPanel.transform);
+        }
+    }
+
+    public void RemoveLife()
+    {
+        if (currentLives > 0)
+        {
+            currentLives--;
+            Transform[] lives = livesPanel.GetComponents<Transform>();
+            Destroy(lives[lives.Length - 1].gameObject);
+        }
+    }
+
+
 }
